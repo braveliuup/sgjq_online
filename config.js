8b4795a3e144f783d033a781b32cc9d690ab5a2f
braveliu <braveliu.site@gmail.com>
2017-03-01 11:00:38 +0800

    x

---
+++ config.js
@@ -0,0 +1,14 @@
+ var config = {
+    state: {
+        pick: '选人状态', 
+        preparing: '准备中',
+        prepared: '已准备',
+        started: '已开始',
+        over: '已结束'
+    },
+    kcharf_emp_db:{
+        'kdr20151005': '刘勇'
+    }    
+}
+
+module.exports = config;
