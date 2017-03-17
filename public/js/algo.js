bc033eec60e5f243d7f557979afa84cc75145a45
braveliuup <braveliu.site@gmail.com>
2017-03-17 19:08:27 +0800

    x

--- public/js/algo.js
+++ public/js/algo.js
@@ -35,7 +35,7 @@ function  getTeamType(type){
     return teamType;
 }
 
-function beginSearch(originCamp, availableCamps){
+function beginSearch(originCamp, availCamps){
     var campLT = chessBoard[originCamp.rowIdx -1][originCamp.colIdx-1];
     var campLB = chessBoard[originCamp.rowIdx -1][originCamp.colIdx+1];
     var campRB = chessBoard[originCamp.rowIdx +1][originCamp.colIdx+1];
@@ -112,22 +112,22 @@ function beginSearch(originCamp, availableCamps){
         if(camp.type == '' ) continue;
         if(!camp.filledChess){
             if(checkOnRailRoad(camp)){
-                var addSuccess = addChessOnRail(camp,availableCamps );
+                var addSuccess = addChessOnRail(camp,availCamps );
                 if(addSuccess){
-                    beginSearch(camp,availableCamps);
+                    beginSearch(camp,availCamps);
                 }
             }else{
                 if(Math.abs(camp.rowIdx - originCamp.rowIdx) <=1 && Math.abs(camp.colIdx - originCamp.colIdx)<=1){
-                    addToAvailCamps(availableCamps,camp);
+                    addToAvailCamps(availCamps,camp);
                 }
             }
         }else{
             if(camp.filledChess.type != firstCamp.filledChess.type && camp.type != '行营'){
                 if(checkOnRailRoad(camp)){
-                        addChessOnRail(camp,availableCamps );
+                        addChessOnRail(camp,availCamps );
                 }else{
                     if(Math.abs(camp.rowIdx - originCamp.rowIdx) <=1 && Math.abs(camp.colIdx - originCamp.colIdx)<=1){
-                        addToAvailCamps(availableCamps,camp);
+                        addToAvailCamps(availCamps,camp);
                     }
                 }
             }
@@ -135,50 +135,217 @@ function beginSearch(originCamp, availableCamps){
     }
 }
 
-function addToAvailCamps(availableCamps,camp){
-    if(availableCamps.indexOf(camp) == -1 && camp.type != ''){
-        availableCamps.push(camp);
+function addToAvailCamps(availCamps,camp){
+    if(availCamps.indexOf(camp) == -1 && camp.type != ''){
+        availCamps.push(camp);
         return true;
     }
     return false;
 }
 
-function addChessOnRail(camp,availableCamps ){
+function addChessOnRail(camp,availCamps, firstCamp ){
     var addSuccess = false;
     if(firstCamp.filledChess.name == '工兵'){
-        addSuccess = addToAvailCamps(availableCamps,camp);
+        addSuccess = addToAvailCamps(availCamps,camp);
     }else{
         if(firstCamp.rowIdx - camp.rowIdx == 0 || firstCamp.colIdx - camp.colIdx ==0 ){
-            addSuccess =   addToAvailCamps(availableCamps,camp);
+            addSuccess =   addToAvailCamps(availCamps,camp);
         }
         /////////////
         if(firstCamp.rowIdx == 6 && camp.colIdx == 6 && firstCamp.colIdx < camp.colIdx && firstCamp.rowIdx > camp.rowIdx){
-            addSuccess =    addToAvailCamps(availableCamps,camp);
+            addSuccess =    addToAvailCamps(availCamps,camp);
         }
         if(firstCamp.colIdx == 6 && camp.rowIdx == 6 && firstCamp.colIdx > camp.colIdx && firstCamp.rowIdx < camp.rowIdx){
-            addSuccess =  addToAvailCamps(availableCamps,camp);
+            addSuccess =  addToAvailCamps(availCamps,camp);
         }
         //////////
         if(firstCamp.rowIdx == 6 && camp.colIdx == 10 && firstCamp.colIdx > camp.colIdx && firstCamp.rowIdx > camp.rowIdx){
-            addSuccess =   addToAvailCamps(availableCamps,camp);
+            addSuccess =   addToAvailCamps(availCamps,camp);
         }
         if(firstCamp.colIdx == 10 && camp.rowIdx == 6 && firstCamp.colIdx < camp.colIdx && firstCamp.rowIdx < camp.rowIdx){
-            addSuccess =   addToAvailCamps(availableCamps,camp);
+            addSuccess =   addToAvailCamps(availCamps,camp);
         }
         ////////////////    
         if(firstCamp.rowIdx == 10 && camp.colIdx == 6 && firstCamp.colIdx < camp.colIdx && firstCamp.rowIdx < camp.rowIdx){
-            addSuccess =  addToAvailCamps(availableCamps,camp);
+            addSuccess =  addToAvailCamps(availCamps,camp);
         }
         if(firstCamp.colIdx == 6 && camp.rowIdx == 10 && firstCamp.colIdx > camp.colIdx && firstCamp.rowIdx > camp.rowIdx){
-            addSuccess =   addToAvailCamps(availableCamps,camp);
+            addSuccess =   addToAvailCamps(availCamps,camp);
         }
         /////////////////
         if(firstCamp.rowIdx == 10 && camp.colIdx == 10 && firstCamp.colIdx > camp.colIdx && firstCamp.rowIdx < camp.rowIdx){
-            addSuccess =   addToAvailCamps(availableCamps,camp);
+            addSuccess =   addToAvailCamps(availCamps,camp);
         }
         if(firstCamp.colIdx == 10 && camp.rowIdx == 10 && firstCamp.colIdx < camp.colIdx && firstCamp.rowIdx > camp.rowIdx){
-            addSuccess =   addToAvailCamps(availableCamps,camp);
+            addSuccess =   addToAvailCamps(availCamps,camp);
         }
     }
     return addSuccess;
+}
+
+function canAttack(camp, originCamp){
+    if(camp.filledChess.type == originCamp.filledChess.type){
+        return false;
+    }
+    var teamType  = getTeamType(originCamp.filledChess.type);
+    if(camp.filledChess.type == teamType){
+        return false;
+    }
+    return true;
+}
+function computeAvailCamp(originCamp){
+    var availCamps = [];
+    var camp = originCamp;
+    var teamType = getTeamType(originCamp.filledChess.type);
+    
+    if(!checkOnRailRoad(originCamp)){
+        var temp = [];
+        if(originCamp.type == '行营'){
+            var campLT = chessBoard[camp.rowIdx -1][camp.colIdx-1];
+            var campLB = chessBoard[camp.rowIdx -1][camp.colIdx+1];
+            var campRB = chessBoard[camp.rowIdx +1][camp.colIdx+1];
+            var campRT = chessBoard[camp.rowIdx +1][camp.colIdx-1];
+            var campU = chessBoard[originCamp.rowIdx-1][originCamp.colIdx];
+            var campB = chessBoard[originCamp.rowIdx+1][originCamp.colIdx];
+            var campL = chessBoard[originCamp.rowIdx][originCamp.colIdx-1];
+            var campR = chessBoard[originCamp.rowIdx][originCamp.colIdx+1];
+            temp = [campU, campB, campL , campR,campLT,campLB, campRB, campRT];
+        }
+        if(originCamp.type == '兵站'){
+            if(originCamp.rowIdx-1 >0){
+                var campU = chessBoard[originCamp.rowIdx-1][originCamp.colIdx];
+                temp.push(campU);
+            }
+            if(originCamp.rowIdx+1 < 17){
+                var campB = chessBoard[originCamp.rowIdx+1][originCamp.colIdx];
+                temp.push(campB);
+            }
+            if(originCamp.colIdx-1 > 0){
+                var campL = chessBoard[originCamp.rowIdx][originCamp.colIdx-1];
+                temp.push(campL);
+            }
+            if(originCamp.colIdx +1 <17){
+                var campR = chessBoard[originCamp.rowIdx][originCamp.colIdx+1];
+                temp.push(campR);
+            }
+        }
+        temp.forEach(function(item){
+            if(!item.filledChess){
+                if(item.type != ''){
+                    availCamps.push(item);
+                }
+            }else{
+                if(item.type == '兵站' && canAttack(item, originCamp)) {
+                    availCamps.push(item);
+                }
+            }
+        });
+        return availCamps;
+    }
+
+    if(checkOnRailRoad(originCamp)){
+        function beginSearch(camp){
+            var ary = [];
+            var campLT = chessBoard[camp.rowIdx -1][camp.colIdx-1];
+            var campLB = chessBoard[camp.rowIdx -1][camp.colIdx+1];
+            var campRB = chessBoard[camp.rowIdx +1][camp.colIdx+1];
+            var campRT = chessBoard[camp.rowIdx +1][camp.colIdx-1];
+
+            [campLT, campLB, campRB, campRT].forEach(function(item, idx){
+                if(item.type == '行营' && item.filledChess == null){
+                    ary.push(item);
+                }
+                var teamType = getTeamType(originCamp.filledChess.type);
+                if(item.filledChess ){
+                    if(item.filledChess.type == originCamp.filledChess.type || item.filledChess.type == teamType){
+                    }else{
+                        ary.push(item);
+                    }
+                }else if(item.type == '兵站'){
+                    ary.push(item);
+                }
+            });
+
+            var campUp, campDown, campLeft, campRight;
+            var campUpRowIdx = camp.rowIdx;
+            var campDownRowIdx = camp.rowIdx;
+            var campLeftColIdx = camp.colIdx;
+            var campRightColIdx = camp.colIdx;
+            while(campUpRowIdx > 0){
+                campUpRowIdx--;
+                campUp = chessBoard[campUpRowIdx][camp.colIdx];
+                if(campUp.type != ''){
+                    break;
+                }
+            }
+            while(campDownRowIdx < 16){
+                campDownRowIdx++;
+                campDown = chessBoard[campDownRowIdx][originCamp.colIdx];
+                if(campDown.type != ''){
+                    break;
+                }
+            }
+            while(campLeftColIdx > 0){
+                campLeftColIdx--;
+                campLeft = chessBoard[originCamp.rowIdx][campLeftColIdx];
+                if(campLeft.type != ''){
+                    break;
+                }
+            }
+            while(campRightColIdx < 16){
+                campRightColIdx++;
+                campRight = chessBoard[originCamp.rowIdx][campRightColIdx];
+                if(campRight.type != ''){
+                    break;
+                }
+            }
+            [campUp, campDown, campLeft, campRight].forEach(function(item, idx){
+                if(item.filledChess){
+                    var teamType = getTeamType(originCamp.filledChess.type);
+                    if(item.filledChess.type == originCamp.filledChess.type || item.filledChess.type == teamType){
+                        return false;
+                    }
+                } else{
+                    if(item.type == ''){
+                        return false;
+                    }
+                }
+                ary.push(item);
+            });
+
+            console.log('对', camp.rowIdx, camp.colIdx,'进行递归，可用营地是');
+            ary.forEach(function(item){
+                console.log('行列:',item.rowIdx+","+item.colIdx);
+            });
+            for(var i = 0; i < ary.length; i++){
+                var camp = ary[i];
+                if(camp.type == '' ) continue;
+                if(!camp.filledChess){
+                    if(checkOnRailRoad(camp)){
+                        var addSuccess = addChessOnRail(camp,availCamps ,originCamp );
+                        if(addSuccess){
+                            beginSearch(camp,availCamps);
+                        }
+                    }else{
+                        if(Math.abs(camp.rowIdx - originCamp.rowIdx) <=1 && Math.abs(camp.colIdx - originCamp.colIdx)<=1){
+                            addToAvailCamps(availCamps,camp);
+                        }
+                    }
+                }else{
+                    if(camp.filledChess.type != teamType && camp.filledChess.type != originCamp.filledChess.type && camp.type != '行营'){
+                        if(checkOnRailRoad(camp)){
+                                addChessOnRail(camp,availCamps ,originCamp);
+                        }else{
+                            if(Math.abs(camp.rowIdx - originCamp.rowIdx) <=1 && Math.abs(camp.colIdx - originCamp.colIdx)<=1){
+                                addToAvailCamps(availCamps,camp);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        beginSearch(originCamp);
+        return availCamps;
+    }
+    return  availCamps;
 }
